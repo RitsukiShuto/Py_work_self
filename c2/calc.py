3d6af8d38063571154ddc0142efb7a5cf6047c24
RitsukiShuto <rtk410s@gmail.com>
2020-03-09 16:20:54 +0900

    Add: chapter2-1, calc1.py calc2.py

---
+++ c2/calc.py
@@ -0,0 +1,7 @@
+# Created by RitsukiShuto on 2020/03/09.
+# Python TextBook Chapter 2-1 "calc.py", "calc2.py"
+#
+print(30 + 5)   # 30 + 5の結果を出力
+print(20 * 3)
+print(2 * 5 // 3)   # "//"で計算結果の小数点以下を切り捨て
+print( (1 + 2) * 3 )
